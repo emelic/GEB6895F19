9ff810ef175bcce3c4a91efbe9246b9ec62e003f
EmeliC <emelic@knights.ucf.edu>
2019-09-17 22:46:55 -0500

    Committing Assignment 1

---
+++ assignment_01/My_Answers/My_AnswersEC.R
@@ -0,0 +1,68 @@
+# Assignment 1
+Example 1:
+
+first_number <- (5)
+second_number <- (6)
+
+sum (first_number*second_number)
+
+print (sum)
+
+Read num1 , num2 
+Set multi to num1*num2 Write multi
+
+Example 2: Write a function that shows the number they entered is not a 5 or 6
+
+is_five_or_six <- function (input) {
+{ result <- 'not five or six'}
+if (input ==5 || input ==6) { result <- 'yes it is a five or six' }
+}
+
+
+Example 3:
+  
+  #Please enter a number
+  num_1 <- (0:10)    
+  num_2 <- (11:20)    
+  num_3 <- (21:30) 
+   if (num_1) <- readline ('blue')
+   if (num_2) <- readline ('red')
+  if (num_3) <- readline ('green')
+   
+  
+  Example 4: 
+  x <- (1:20)     
+  sum(x*5)
+  
+  
+  Example 5: 
+  x <- 0 (x < count)     
+  even <- even + 2     
+  x = x + 1     
+  even 
+  
+  Example 6:
+  #"please enter 5 numbers" 
+  values <- (n1,n2,n3,n4,n5) 
+  # "The average is" 
+  avg <- (n1+n2+n3+n4+n5)/5 
+   avg 
+ 
+If(n1 < n2)  
+ max <- n2 Else   
+ max <- n1 If(n3 > max)   
+ max <- n3 If(n4 > max)  
+ max <- n4 If(n5 > max) 
+ max to n5 
+#The max is"  
+ If(n1 > n2)   
+ min <- n2 Else   
+ min <- n1 If(n3 < min)   
+ min <- n3 If(n4 < min)   
+ min <- n4 If(n5 < min) 
+ min <- n5 
+ #"The min is"  
+
+
+
+
